1b2d92fcc52e805f1cd03d06faaea28b179d4baa
sakovets <skystalker01@gmail.com>
2023-04-20 15:06:27 +0300

    comment

--- pyton-les4.py
+++ pyton-les4.py
@@ -1,1 +1,16 @@
-print("lesonn 4 \n\n")
+# fizz-buzz
+
+fizz = int(input('Введіть число fizz\n'))
+buzz = int(input('Введіть число buzz\n'))
+num = int(input('Введіть число num\n'))
+
+for i in range(1, num+1):
+   if not i%fizz and not i%buzz:
+        print("FB", end=" ")
+   elif not i%fizz  and i%buzz:
+        print("F", end=" ")
+   elif i%fizz  and not i%buzz:
+        print("B", end=" ")
+   else :
+        print(i, end=" ")    
+
